a31f4f3980ce84738a40745926f63bdf5aa309ef
SivaSankar Cheekati <siva.sankar@clairvoyantsoft.com>
2016-09-30 08:12:38 +0530

    airflow sdl and script files

---
+++ src/scripts/airflow.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+useradd -G supergroup airflow
+
+# airflow needs a home, ~/airflow is the default,
+# but you can lay foundation somewhere else if you prefer
+# (optional)
+export AIRFLOW_HOME=~/airflow
+
+# install from pypi using pip
+pip install airflow
+
+# initialize the database
+airflow initdb
+
+# start the web server, default port is 8080
+airflow webserver -p 8080
