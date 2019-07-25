f0d3700c0abe7f4e5d5dc131e27dec138c72d1f7
csistla <csistla@hawk.iit.edu>
2019-07-25 16:38:13 -0700

    Update mkuser.py

--- src/scripts/mkuser.py
+++ src/scripts/mkuser.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # https://airflow.incubator.apache.org/security.html
 
-import sys, logging
+import sys, logging, argparse
 
 from airflow import models, settings
 from airflow.contrib.auth.backends.password_auth import PasswordUser
@@ -11,6 +11,18 @@ email = sys.argv[2]
 password = sys.argv[3]
 
 session = settings.Session()
+
+#add any needed parameters if necessary
+parser=argparse.ArgumentParser(
+    description='''Create users with airflow create_user. ''',
+    epilog="""Please refer apache-airflow for more args!.""")
+parser.add_argument('-r', type=int, default=42, help='ROLE!')
+parser.add_argument('-u', type=int, default=42, help='USERNAME!')
+parser.add_argument('-e', type=int, default=42, help='EMAIL!')
+parser.add_argument('-f', type=int, default=42, help='FIRSTNAME!')
+parser.add_argument('-l', type=int, default=42, help='LASTNAME!')
+parser.add_argument('-p', type=int, default=42, help='PASSWORD!')
+args=parser.parse_args()
 
 def is_user_exists(username):
     return (session.query(models.User).filter(models.User.username == username).first() != None)
